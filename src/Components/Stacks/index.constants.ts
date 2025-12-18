2e32d523a8b388115a740b6d259a5414286cb1c9
Najaxx123 <romain.brival@outlook.fr>
2025-12-18 15:43:30 +0100

    test stacks n°1

--- src/Components/Stacks/index.constants.ts
+++ src/Components/Stacks/index.constants.ts
@@ -1,19 +1,15 @@
+
 export interface StackProps {
   name: string;
   goodAt: 'advanced' | 'intermediate' | 'beginner';
 }
 
 export const stacks: Array<StackProps> = [
-  { name: 'React', goodAt: 'advanced' },
-  { name: 'Typescript', goodAt: 'advanced' },
-  { name: 'NextJS', goodAt: 'advanced' },
-  { name: 'React Native', goodAt: 'advanced' },
+  { name: 'Html', goodAt: 'advanced'},
+  { name: 'Css', goodAt: 'advanced'},
   { name: 'Javascript', goodAt: 'advanced' },
-  { name: 'Jest', goodAt: 'advanced' },
-  { name: 'StoryBook', goodAt: 'advanced' },
-  { name: 'SASS', goodAt: 'advanced' },
   { name: 'PHP', goodAt: 'intermediate' },
-  { name: 'NodeJS', goodAt: 'intermediate' },
   { name: 'SQL', goodAt: 'intermediate' },
-  { name: 'Python', goodAt: 'beginner' },
+  { name: 'React', goodAt: 'beginner' },
+  { name: 'NodeJS', goodAt: 'beginner' }
 ];
